365a57b6018f01849ccd38b80d0101bf087447fe
Natan Anael <natan.anael@gmail.com>
2020-03-01 18:44:32 -0300

    Change debian to  ubuntu ansible folder

---
+++ ubuntu/installer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+## We need the current user name to setup the tools
+export CURRENT_USER="$(whoami)"
+
+## Update cache and install Git and Ansible
+apt-get update
+apt-get install -y ansible git
+
+## Installing Ansible and Git
+cd /tmp/
+git clone https://github.com/wesleyit/ansible_datascience.git
+cd ansible_datascience
+
+## Running Ansible
+echo "Hello! It is time to install some packages.
+It will take a long time, go take a coffee (a long one)."
+
+ansible-playbook -i debian/.ansible-hosts playbook.yml
